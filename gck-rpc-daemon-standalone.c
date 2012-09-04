3312d8988318bb37dc5696ac001eb2619704deaf
Leif Johansson <leifj@sunet.se>
2012-09-04 12:21:50 +0200

    basic seccmp filter

--- gck-rpc-daemon-standalone.c
+++ gck-rpc-daemon-standalone.c
@@ -42,6 +42,63 @@
 
 #define SOCKET_PATH "tcp://127.0.0.1"
 
+#include "seccomp-bpf.h"
+#include "syscall-reporter.h"
+
+static int install_syscall_filter(void)
+{
+	struct sock_filter filter[] = {
+	        /* Validate architecture. */
+		VALIDATE_ARCHITECTURE,
+		/* Grab the system call number. */
+		EXAMINE_SYSCALL,
+		/* List allowed syscalls. */
+		ALLOW_SYSCALL(rt_sigreturn),
+#ifdef __NR_sigreturn
+		ALLOW_SYSCALL(sigreturn),
+#endif
+		ALLOW_SYSCALL(exit_group),
+		ALLOW_SYSCALL(exit),
+		ALLOW_SYSCALL(read),
+		ALLOW_SYSCALL(write),
+                ALLOW_SYSCALL(futex),
+                ALLOW_SYSCALL(brk),
+                ALLOW_SYSCALL(open),
+                ALLOW_SYSCALL(fstat64),
+                ALLOW_SYSCALL(mmap2),
+                ALLOW_SYSCALL(mprotect),
+                ALLOW_SYSCALL(close),
+                ALLOW_SYSCALL(access),
+                ALLOW_SYSCALL(munmap),
+                ALLOW_SYSCALL(time),
+                ALLOW_SYSCALL(_llseek),
+                ALLOW_SYSCALL(stat64),
+                ALLOW_SYSCALL(fcntl64),
+                ALLOW_SYSCALL(mlock),
+                ALLOW_SYSCALL(munlock),
+		KILL_PROCESS,
+	};
+	struct sock_fprog prog = {
+		.len = (unsigned short)(sizeof(filter)/sizeof(filter[0])),
+		.filter = filter,
+	};
+
+	if (prctl(PR_SET_NO_NEW_PRIVS, 1, 0, 0, 0)) {
+		perror("prctl(NO_NEW_PRIVS)");
+		goto failed;
+	}
+	if (prctl(PR_SET_SECCOMP, SECCOMP_MODE_FILTER, &prog)) {
+		perror("prctl(SECCOMP)");
+		goto failed;
+	}
+	return 0;
+
+failed:
+	if (errno == EINVAL)
+		fprintf(stderr, "SECCOMP_FILTER is not available. :(\n");
+	return 1;
+}
+
 #if 0
 /* Sample configuration for loading NSS remotely */
 static CK_C_INITIALIZE_ARGS p11_init_args = {
@@ -71,6 +128,12 @@ int main(int argc, char *argv[])
 	fd_set read_fds;
 	int sock, ret;
 	CK_RV rv;
+
+
+        if (install_syscall_reporter())
+                return 1;
+        if (install_syscall_filter())
+        	return 1;
 
 	/* The module to load is the argument */
 	if (argc != 2 && argc != 3)
